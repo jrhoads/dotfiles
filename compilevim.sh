5ef2a51e5f97723542e8ef016a9cac39c7d2c3ca
jrhoads <joseph_rhoads@brown.edu>
2021-09-30 20:48:57 -0400

    Update vim installer

--- compilevim.sh
+++ compilevim.sh
@@ -1,9 +1,22 @@
 mkdir -p $HOME/Installs/
 cd $HOME/Installs
-git clone https://github.com/vim/vim.git
+if [ -d vim ]; then
+	rm -rf vim
+fi
+git clone --depth 1 https://github.com/vim/vim.git
 cd vim
-
-./configure --disable-perlinterp --enable-pythoninterp --enable-python3interp --enable-multibyte --disable-gui --prefix=$HOME/.local/vim --with-features=huge --with-tlib=ncurses --without-x
+./configure --disable-perlinterp \
+	--enable-pythoninterp \
+	--with-python-config-dir=$HOME/miniconda3/envs/py27/lib/python2.7/config \
+	--enable-python3interp \
+	--with-python3-config-dir=$HOME/miniconda3/envs/py36/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+	--enable-multibyte \
+	--disable-gui \
+	--prefix=$HOME/.local/vim \
+	--with-features=huge \
+	--with-tlib=ncurses \
+	--enable-cscope \
+	--without-x
 make && make install
 cd $HOME/.local/bin
 ln -s ../vim/bin/vim
