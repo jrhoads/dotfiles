aa5d3cfed13b2d7e58d8bd14b5c3a4c2ec664b7e
jrhoads <jrhoads@datacite.org>
2024-04-08 05:41:25 -0400

    Cleanup spacing

--- config/nvim/lua/plugins/vimux.lua
+++ config/nvim/lua/plugins/vimux.lua
@@ -4,7 +4,5 @@ return {
 		vim.g.VimuxOrientation = "hb"
 		vim.keymap.set('n', '<leader>z', ":VimuxTogglePane<cr>", { silent = true })
 		vim.keymap.set('n', '<leader>vo', ":VimuxOpenRunner<cr>", { silent = true })
-
-
 	end
 }
