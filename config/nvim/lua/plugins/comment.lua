3fba437fd92c140b1691213a03ab3e409ba323cc
jrhoads <jrhoads@datacite.org>
2024-04-26 05:32:28 -0400

    Add ts commentstring to comment plugin

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,18 @@
 return {
 	{'numToStr/Comment.nvim',
 	lazy = false,
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
 	config = function()
-		require('Comment').setup()
+		local comment = require("Comment")
+
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+		comment.setup()
+		comment.setup({
+			pre_hook = ts_context_commentstring.create_pre_hook(),
+		})
 		vim.keymap.set('n', '<leader>cc', function()
 			return vim.v.count == 0
 			and '<Plug>(comment_toggle_linewise_current)'
