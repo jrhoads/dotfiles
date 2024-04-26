1197a26c0c8aec0a26bf9d405dcfd4d01f0aefcc
jrhoads <jrhoads@datacite.org>
2024-04-26 05:31:23 -0400

    Formatting

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -1,15 +1,15 @@
-return{
-	{
-		'akinsho/bufferline.nvim',
-		version = "*",
-		dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      require("bufferline").setup({
-        options = {
-          separator_style = 'slant',
+return {
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup({
+                options = {
+                    separator_style = 'slant',
 
-        }
-      })
-    end
-	}
+                }
+            })
+        end
+    }
 }
--- config/nvim/lua/plugins/codeium.lua
+++ config/nvim/lua/plugins/codeium.lua
@@ -1,12 +1,12 @@
 return {
-	'Exafunction/codeium.vim',
-	config = function()
-	vim.g.surround_no_insert_mappings = 1
-	vim.g.codeium_disable_mappings = 1
-	vim.g.codeium_no_map_tab = 1
-	vim.keymap.set('i', '<C-g>', "codeium#Accept()",
-	{ silent = true, script = true, nowait= true, expr = true })
-	vim.keymap.set('i', '<C-b>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    'Exafunction/codeium.vim',
+    config = function()
+        vim.g.surround_no_insert_mappings = 1
+        vim.g.codeium_disable_mappings = 1
+        vim.g.codeium_no_map_tab = 1
+        vim.keymap.set('i', '<C-g>', "codeium#Accept()",
+        { silent = true, script = true, nowait= true, expr = true })
+        vim.keymap.set('i', '<C-b>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
 
-	end
+    end
 }
