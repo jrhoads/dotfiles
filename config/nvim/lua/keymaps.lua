160d94a8567510ca65b3d8492464b3e51bdd3b76
jrhoads <jrhoads@datacite.org>
2024-07-04 13:41:20 +0200

    More Keymaps

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,24 +1,39 @@
-vim.keymap.set('n', '<Leader><TAB>', '<C-W>W', { silent = true })
-vim.keymap.set('n', '<Leader><S-TAB>', '<C-W>p', { silent = true })
-vim.keymap.set('n', '<Leader><Space>', ':bn<cr>', { silent = true })
-vim.keymap.set('n', '<Leader><Leader>', ':bp<cr>', { silent = true })
-vim.keymap.set('n', '<Leader><BS>', ':bp<cr>', { silent = true })
+local keyset = vim.keymap.set
 
-vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+keyset('n', '<Leader><TAB>', '<C-W>W', { silent = true })
+keyset('n', '<Leader><S-TAB>', '<C-W>p', { silent = true })
+keyset('n', '<Leader><Space>', ':bn<cr>', { silent = true })
+keyset('n', '<Leader><Leader>', ':bp<cr>', { silent = true })
+keyset('n', '<Leader><BS>', ':bp<cr>', { silent = true })
+
+keyset('n', '<C-h>', '<C-w>h', { silent = true })
+keyset('n', '<C-j>', '<C-w>j', { silent = true })
+keyset('n', '<C-k>', '<C-w>k', { silent = true })
+keyset('n', '<C-l>', '<C-w>l', { silent = true })
 
 
-vim.keymap.set('n', '<F1>', '<ESC>')
-vim.keymap.set('i', '<F1>', '<ESC>')
-vim.keymap.set('v', '<F1>', '<ESC>')
+keyset('n', '<F1>', '<ESC>')
+keyset('i', '<F1>', '<ESC>')
+keyset('v', '<F1>', '<ESC>')
 
 -- Escape from the home row
-vim.keymap.set('i', 'jj', '<Esc>')
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('i', 'kj', '<Esc>')
-vim.keymap.set('i', 'kk', '<Esc>')
+keyset('i', 'jj', '<Esc>')
+keyset('i', 'jk', '<Esc>')
+keyset('i', 'kj', '<Esc>')
+keyset('i', 'kk', '<Esc>')
 
-vim.keymap.set('n', '<Space>', 'za', { noremap = true })
-vim.keymap.set('n', '<Leader>ll', ':set list!<CR>', { noremap = true, silent = true })
+keyset('n', '<Space>', 'za', { noremap = true })
+keyset('n', '<Leader>ll', ':set list!<CR>', { noremap = true, silent = true })
+
+-- resize windows easily
+keyset("n", "<down>", ":resize +2<cr>")
+keyset("n", "<up>", ":resize -2<cr>")
+keyset("n", "<right>", ":vertical resize +2<cr>")
+keyset("n", "<left>", ":vertical resize -2<cr>")
+
+-- movement while in isert mode
+keyset("i", "<C-h>", "<left>", { noremap = true })
+keyset("i", "<C-j>", "<down>", { noremap = true })
+keyset("i", "<C-k>", "<up>", { noremap = true })
+keyset("i", "<C-l>", "<right>", { noremap = true })
+
