00c1c080e4c653f7898794967c144f35e4b28e36
jrhoads <jrhoads@datacite.org>
2024-10-03 17:53:26 +0200

    Remap telescope bindings

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -66,6 +66,24 @@ return {
                     "--glob=!**/yarn.lock",
                     "--glob=!**/package-lock.json",
                 }, -- find_command
+		mappings = {
+			
+			i = {
+				[".."] = function(prompt_bufnr)
+					local current_picker =
+						require("telescope.actions.state").get_current_picker(prompt_bufnr)
+					-- cwd is only set if passed as telescope option
+					local cwd = current_picker.cwd and tostring(current_picker.cwd)
+						or vim.loop.cwd()
+					local parent_dir = vim.fs.dirname(cwd)
+					require("telescope.actions").close(prompt_bufnr)
+					require("telescope.builtin").find_files {
+						prompt_title = vim.fs.basename(parent_dir),
+						cwd = parent_dir,
+					}
+				end
+			}
+		}
             }, -- find_files
             colorscheme = {
                 enable_preview = true
@@ -73,16 +91,52 @@ return {
         }, -- pickers
     }
 
+    local my_grep = function()
+	    local cword = vim.fn.expand("<cword>")
+	    require("telescope.builtin").live_grep({
+		    default_text = cword,
+		    on_complete = cword ~= "" and {
+			    function(picker)
+				    local mode = vim.fn.mode()
+				    local keys = mode ~= "n" and "<ESC>" or ""
+				    vim.api.nvim_feedkeys(
+				    vim.api.nvim_replace_termcodes(keys .. [[^v$<C-g>]], true, false, true),
+				    "n",
+				    true
+				    )
+				    -- should you have more callbacks, just pop the first one
+				    table.remove(picker._completion_callbacks, 1)
+				    -- copy mappings s.t. eg <C-n>, <C-p> works etc
+				    vim.tbl_map(function(mapping)
+					    vim.api.nvim_buf_set_keymap(0, "s", mapping.lhs, mapping.rhs, {})
+				    end, vim.api.nvim_buf_get_keymap(0, "i"))
+			    end,
+		    } or nil,
+	    })
+    end
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>r', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>g', builtin.treesitter, {})
+    vim.keymap.set('n', '<leader>fg', builtin.treesitter, {})
     vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})
+    vim.keymap.set('n', '<leader>*', my_grep, {})
     -- vim.keymap.set('n', '/', builtin.current_buffer_fuzzy_find, {})
     vim.keymap.set('n', '<leader>uu', builtin.colorscheme, {})
     vim.keymap.set('n', '\\', builtin.live_grep, {})
 end
-  }
+  },
+  --lazy
+{
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+	    require("telescope").load_extension "file_browser"
+	    vim.keymap.set('n', '<leader>ff', require('telescope').extensions.file_browser.file_browser, {})
+    end
+
+
+}
 }
