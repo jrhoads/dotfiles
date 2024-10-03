f5c4064018cbb15daeeb327ba2454cc1da2b2284
jrhoads <jrhoads@datacite.org>
2024-10-03 17:59:14 +0200

    Add another binding to toggleterm

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -27,10 +27,12 @@ return {
                 vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
                 vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
                 vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+                vim.api.nvim_buf_set_keymap(0, 't', '<C-t>', [[<C-\><C-n><C-W>l]], opts)
             end
 
             vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
             vim.keymap.set('n', '<m-`>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+            vim.keymap.set('n', '<c-t>', ':ToggleTerm<CR>', { noremap = true, silent = true })
 	    local Terminal  = require('toggleterm.terminal').Terminal
 	    local lazygit = Terminal:new({
 		    cmd = "lazygit",
