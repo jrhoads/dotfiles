2a078742e3aec56125cca2d85a01675ca72c1c08
jrhoads <jrhoads@datacite.org>
2024-10-07 10:48:04 +0200

    Update nvim ui

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -3,10 +3,13 @@ return {
         'akinsho/bufferline.nvim',
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons',
+	enabled = true,
         config = function()
             require("bufferline").setup({
                 options = {
-                    separator_style = 'slant',
+			separator_style = 'slant',
+			show_buffer_close_icons = false,
+			show_close_icon = false,
 
                 }
             })
--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -1,76 +1,57 @@
 -- Convert my other plugins to lazy.nvim
 return {
-    { 'ap/vim-css-color' },
-    { 'thinca/vim-visualstar' },
-    { 'tmhedberg/SimpylFold' },
-    { 'Yggdroot/hiPairs' },
-    {
-        'vim-test/vim-test',
-        config = function()
-            vim.keymap.set('n', '<leader>tm', ':TestNearest<cr>')
-            vim.keymap.set('n', '<leader>tf', ':TestFile<cr>')
-            vim.keymap.set('n', '<leader>ts', ':TestSuite<cr>')
-        end
-    },
-    {
-        'dense-analysis/ale',
-        config = function()
-            vim.g.ale_sign_error = '✘'
-            vim.g.ale_sign_warning = '⚠'
-            vim.g.ale_fix_on_save = 1
-	    vim.g.ale_linters = {
-		    python = { 'ruff', },
-		    javascript = { 'eslint' },
-		    typescript = { 'eslint' },
-		    graphql = { 'eslint' },
-	    }
-	    vim.g.ale_fixers = {
-		    python = { 'black', 'isort'},
-		    javascript = { 'prettier', 'eslint' },
-		    typescript = { 'prettier', 'eslint' },
-	    }
-        end
-    },
-    {
-        'AndrewRadev/splitjoin.vim',
-        config = function()
-            vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-            vim.g.splitjoin_trailing_comma = 1
-            vim.g.splitjoin_python_brackets_on_separate_lines = 1
-            vim.keymap.set('n', 'sk', ':SplitjoinSplit<cr>')
-            vim.keymap.set('n', 'sj', ':SplitjoinJoin<cr>')
-        end
-    },
-    {
-        'sedm0784/vim-resize-mode',
-        config = function()
-            -- When vim is resized, equalize the splits
-            vim.api.nvim_create_autocmd({ "VimResized" }, {
-                pattern = "*",
-                command = "wincmd ="
-            })
-        end
-    },
-    {
-        "folke/zen-mode.nvim",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        config = function()
-            require("zen-mode").setup {
-                vim.keymap.set('n', '<leader>m', ":ZenMode<CR>", { silent = true }),
-            }
-        end
-    },
-    {
-        'szw/vim-maximizer',
-        config = function()
-            vim.keymap.set('n', '<leader>w', ":MaximizerToggle<CR>", { silent = true })
-            vim.keymap.set('n', '<C-Space>', ":MaximizerToggle<CR>", { silent = true })
-            vim.keymap.set('v', '<C-Space>', ":MaximizerToggle<CR>gv", { silent = true })
-            vim.keymap.set('i', '<C-Space>', "<C-o>:MaximizerToggle<CR>", { silent = true })
-        end
-    },
+  { 'ap/vim-css-color' },
+  { 'thinca/vim-visualstar' },
+  -- fastFold (https://github.com/Konfekt/FastFold)
+  { 'Konfekt/FastFold' },
+  -- simplyFold (https://github.com/tmhedberg/SimpylFold)
+  { 'tmhedberg/SimpylFold' },
+  -- vim-test (https://github.com/vim-test/vim-test)
+  {
+    'vim-test/vim-test',
+    config = function()
+      vim.keymap.set('n', '<leader>tm', ':TestNearest<cr>')
+      vim.keymap.set('n', '<leader>tf', ':TestFile<cr>')
+      vim.keymap.set('n', '<leader>ts', ':TestSuite<cr>')
+    end
+  },
+  {
+    'dense-analysis/ale',
+    config = function()
+      vim.g.ale_sign_error = '✘'
+      vim.g.ale_sign_warning = '⚠'
+      vim.g.ale_fix_on_save = 1
+      vim.g.ale_linters = {
+        python = { 'ruff', },
+        javascript = { 'eslint' },
+        typescript = { 'eslint' },
+        graphql = { 'eslint' },
+      }
+      vim.g.ale_fixers = {
+        python = { 'black', 'isort'},
+        javascript = { 'prettier', 'eslint' },
+        typescript = { 'prettier', 'eslint' },
+      }
+    end
+  },
+  {
+    'AndrewRadev/splitjoin.vim',
+    config = function()
+      vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+      vim.g.splitjoin_trailing_comma = 1
+      vim.g.splitjoin_python_brackets_on_separate_lines = 1
+      vim.keymap.set('n', 'sk', ':SplitjoinSplit<cr>')
+      vim.keymap.set('n', 'sj', ':SplitjoinJoin<cr>')
+    end
+  },
+  {
+    'sedm0784/vim-resize-mode',
+    config = function()
+      -- When vim is resized, equalize the splits
+      vim.api.nvim_create_autocmd({ "VimResized" }, {
+        pattern = "*",
+        command = "wincmd ="
+      })
+    end
+  },
 }
