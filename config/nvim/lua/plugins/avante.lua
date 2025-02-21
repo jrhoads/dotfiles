6c6a05af5ae6372e6a2a5bf994d5ed1f42d3c373
jrhoads <jrhoads@datacite.org>
2025-02-21 16:06:00 +0100

    feat(avante): improve plugin setup

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -2,7 +2,8 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = 'v0.0.9', -- set to last release before winfixbuf
+  -- version = 'v0.0.9', -- set to last release before winfixbuf
+  version = '*', -- set to last release before winfixbuf
   opts = {
     provider = "claude",
     claude = {
@@ -14,37 +15,35 @@ return {
         insert = "<C-g><C-g>",
       },
     },
-   windows = {
-    ---@type "right" | "left" | "top" | "bottom"
-    position = "right", -- the position of the sidebar
-    wrap = true, -- similar to vim.o.wrap
-    width = 30, -- default % based on available width
-    sidebar_header = {
-      enabled = true, -- true, false to enable/disable the header
-      align = "center", -- left, center, right for title
-      rounded = true,
+    windows = {
+      ---@type "right" | "left" | "top" | "bottom"
+      position = "right", -- the position of the sidebar
+      wrap = true,      -- similar to vim.o.wrap
+      width = 30,       -- default % based on available width
+      sidebar_header = {
+        enabled = true, -- true, false to enable/disable the header
+        align = "center", -- left, center, right for title
+        rounded = true,
+      },
+      input = {
+        prefix = "> ",
+        height = 8, -- Height of the input window in vertical layout
+      },
+      edit = {
+        border = "rounded",
+        start_insert = true, -- Start insert mode when opening the edit window
+      },
+      ask = {
+        floating = false,  -- Open the 'AvanteAsk' prompt in a floating window
+        start_insert = true, -- Start insert mode when opening the ask window
+        border = "rounded",
+        ---@type "ours" | "theirs"
+        focus_on_apply = "ours", -- which diff to focus after applying
+      },
+      winfixbuf = false,
     },
-    input = {
-      prefix = "> ",
-      height = 8, -- Height of the input window in vertical layout
-    },
-    edit = {
-      border = "rounded",
-      start_insert = true, -- Start insert mode when opening the edit window
-    },
-    ask = {
-      floating = false, -- Open the 'AvanteAsk' prompt in a floating window
-      start_insert = true, -- Start insert mode when opening the ask window
-      border = "rounded",
-      ---@type "ours" | "theirs"
-      focus_on_apply = "ours", -- which diff to focus after applying
-    },
-    winfixbuf=false,
   },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
@@ -52,23 +51,6 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    -- {
-    --   -- support for image pasting
-    --   "HakonHarnes/img-clip.nvim",
-    --   event = "VeryLazy",
-    --   opts = {
-    --     -- recommended settings
-    --     default = {
-    --       embed_image_as_base64 = false,
-    --       prompt_for_file_name = false,
-    --       drag_and_drop = {
-    --         insert_mode = true,
-    --       },
-    --       -- required for Windows users
-    --       use_absolute_path = true,
-    --     },
-    --   },
-    -- },
     {
       -- Make sure to set this up properly if you have lazy=true
       'MeanderingProgrammer/render-markdown.nvim',
