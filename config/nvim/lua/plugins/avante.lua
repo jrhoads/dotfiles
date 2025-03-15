8f0a452adcd742e9c17ead932fdd10a2364f5314
jrhoads <jrhoads@datacite.org>
2025-03-15 12:01:43 +0100

    Update avante.nvim plugin configuration

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -1,19 +1,27 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  lazy = false,
+  --   keys = {
+  --       { "<leader>aa", "<cmd>AvanteAsk<cr>", mode = "n", desc = "Avante Ask" },
+  --       { "<leader>aa", ":<C-u>AvanteAsk<cr>", mode = "v", desc = "Avante Ask Selection" },
+  --   },
+  -- lazy = true,
   -- version = 'v0.0.9', -- set to last release before winfixbuf
   version = '*', -- NOTE: When using newer versions, you must edit sidebar.lua to comment out winfixbuf
   opts = {
     provider = "claude",
     claude = {
       api_key_name = { "op.exe", "read", "op://Private/Antropic-NVIM/credential" },
+      model = "claude-3-5-sonnet-20241022",
     },
     mappings = {
       submit = {
         normal = "<CR>",
         insert = "<C-g><C-g>",
       },
+      sidebar = {
+        close_from_input = { normal = "<Esc>", insert = "<C-d>" },
+      }
     },
     windows = {
       ---@type "right" | "left" | "top" | "bottom"
