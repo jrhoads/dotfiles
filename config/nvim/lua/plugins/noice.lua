6974830f84068efe804cda466d61e417222536e8
jrhoads <jrhoads@datacite.org>
2025-03-26 09:58:26 +0100

    feat: Add nvim-notify dependency to noice.nvim

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -2,6 +2,7 @@ return{
 	'folke/noice.nvim',
 	dependencies = {
 		{ 'MunifTanjim/nui.nvim' },
+		{ 'rcarriga/nvim-notify' },
 	},
 	config = function()
 		require("noice").setup({
@@ -38,4 +39,3 @@ return{
 		})
 	end
 }
-
