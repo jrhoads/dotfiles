a54916d48603ae35210922dd63f6bad0b3759610
jrhoads <jrhoads@datacite.org>
2025-03-26 09:57:12 +0100

    feat: configure lualine with separators, diagnostics, and other components

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,23 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "wombat",
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "wombat",
+        section_separators = "",
+        component_separators = "",
+        globalstatus = true,
+        icons_enabled = true,
+      },
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
