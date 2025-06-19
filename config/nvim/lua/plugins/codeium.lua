9cdcddda5d81265587087b2a28550dcfb598c6e5
jrhoads <jrhoads@datacite.org>
2025-06-19 10:48:32 +0200

    refactor: Migrate codeium.nvim to windsurf.nvim and config setup

--- config/nvim/lua/plugins/codeium.lua
+++ config/nvim/lua/plugins/codeium.lua
@@ -1,29 +1,31 @@
   return {
-    "Exafunction/codeium.nvim",
+    "Exafunction/windsurf.nvim",
     event = "BufEnter",
     enabled=true,
     dependencies = {
       "nvim-lua/plenary.nvim",
       -- "hrsh7th/nvim-cmp",
     },
-    opts = {
-      enable_chat = false,
-      enable_cmp_source = true,
-      surround_no_insert_mappings = true,
-      -- map_keys = false,
-      virtual_text = {
-        enabled = true,
-        map_keys = true,
-        key_bindings = {
-          -- Accept the current completion.
-          accept = "<C-g>",
-          -- Accept the current completion and move to the next one.
-          next = "<C-b>",
-          -- Accept the current completion and move to the previous one.
-          prev = "<C-f>",
-          -- Dismiss the current completion.
-          clear = "<C-x>",
-        },
-      }
-    },
+    config = function()
+      require("codeium").setup({
+        enable_chat = false,
+        enable_cmp_source = true,
+        surround_no_insert_mappings = true,
+        -- map_keys = false,
+        virtual_text = {
+          enabled = true,
+          map_keys = true,
+          key_bindings = {
+            -- Accept the current completion.
+            accept = "<C-g>",
+            -- Accept the current completion and move to the next one.
+            next = "<C-b>",
+            -- Accept the current completion and move to the previous one.
+            prev = "<C-f>",
+            -- Dismiss the current completion.
+            clear = "<C-x>",
+          },
+        }
+      })
+    end
   }
