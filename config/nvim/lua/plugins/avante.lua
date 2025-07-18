ee233a0bfa4c978eb2e7818116b938b185b40af1
jrhoads <jrhoads@datacite.org>
2025-07-18 15:31:45 +0200

    Update avante config

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -1,46 +1,54 @@
 return {
   "yetone/avante.nvim",
-  event = "VeryLazy",
-  -- lazy = true,
+  -- event = "VeryLazy",
+  lazy = false,
   -- version = 'v0.0.9', -- set to last release before winfixbuf
   version = '*', -- NOTE: When using newer versions, you must edit sidebar.lua to comment out winfixbuf
+  cmd = { "AvanteAsk", "AvanteToggle", "AvanteChat" },
   opts = {
-    provider = "claude",
+    mode = "legacy",
     provider = "gemini",
+    -- cursor_applying_provider = "openrouter",
     auto_suggestions_provider = nil,
-    vendors = {
-      openrouter = {
-        __inherited_from = 'openai',
-        endpoint = 'https://openrouter.ai/api/v1',
-        api_key_name = 'cmd:op.exe read op://Private/OpenRouter-NVIM/credential',
-        model = 'google/gemini-2.5-flash-preview-05-20',
+    providers ={
+      -- openrouter = {
+      --   __inherited_from = 'openai',
+      --   endpoint = 'https://openrouter.ai/api/v1',
+      --   api_key_name = 'cmd:op read op://Private/OpenRouter-NVIM/credential',
+      --   model = "gemini-2.5-flash-lite-preview-06-17",
+      -- },
+      -- claude = {
+      --   endpoint = "https://api.anthropic.com",
+      --   model = "claude-sonnet-4-20250514",
+      --   api_key_name = 'cmd:op read op://Private/Antropic-NVIM/credential',
+      --   timeout = 30000, -- Timeout in milliseconds
+      --   disable_tools = true,
+      --     extra_request_body = {
+      --       temperature = 0.75,
+      --       max_tokens = 20480,
+      --     },
+      -- },
+      gemini = {
+        endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+        -- model = "gemini-2.0-flash",
+        model = "gemini-2.5-flash-lite-preview-06-17",
+        -- model = "gemini-2.5-flash-preview-04-17",
+        -- model = "gemini-2.5-pro-preview-05-06",
+        api_key_name = "cmd:op read op://Private/Gemini-NVIM/credential",
+        timeout = 5000, -- Timeout in milliseconds
+        disable_tools = true,
+        extra_request_body = {
+          temperature = 0,
+          max_tokens = 1048576,
+        },
       },
-    },
-    claude = {
-      endpoint = "https://api.anthropic.com",
-      -- model = "claude-3-7-sonnet-20250219",
-      model = "claude-3-5-sonnet-20241022",
-      api_key_name = "cmd:op.exe read op://Private/Antropic-NVIM/credential",
-      temperature = 0,
-      max_tokens = 8192,
-      disable_tools = true,
-    },
-    gemini = {
-      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-      -- model = "gemini-2.0-flash",
-      model = "gemini-2.5-flash-lite-preview-06-17",
-      -- model = "gemini-2.5-flash-preview-04-17",
-      -- model = "gemini-2.5-pro-preview-05-06",
-      api_key_name = "cmd:op.exe read op://Private/Gemini-NVIM/credential",
-      timeout = 30000, -- Timeout in milliseconds
-      temperature = 0,
-      max_tokens = 1048576,
     },
     behavior = {
       auto_suggestions = false,
       auto_set_highlight_group = true,
       minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
-      enable_token_counting = true
+      enable_token_counting = true,
+      -- enable_cursor_planning_mode = true
     },
     mappings = {
       submit = {
