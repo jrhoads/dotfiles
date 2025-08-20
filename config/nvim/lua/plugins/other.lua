7edc9720459d13084a8450ca0193868c2779f1c3
jrhoads <jrhoads@datacite.org>
2025-08-20 23:38:56 +0200

    refactor: Remove ALE plugin configuration

--- config/nvim/lua/plugins/other.lua
+++ config/nvim/lua/plugins/other.lua
@@ -30,55 +30,6 @@ return {
     end
   },
   {
-    'dense-analysis/ale',
-    enabled = false,
-    desc = "Asynchronous Lint Engine",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      -- Signs and UI
-      vim.g.ale_sign_error = '✘'
-      vim.g.ale_sign_warning = '⚠'
-      vim.g.ale_sign_info = '🛈'
-      vim.g.ale_sign_style_error = '💡'
-      vim.g.ale_sign_style_warning = '⚡'
-
-      -- Performance settings
-      vim.g.ale_fix_on_save = 1
-      vim.g.ale_lint_on_enter = 0
-      vim.g.ale_lint_on_insert_leave = 0
-      vim.g.ale_lint_on_filetype_changed = 0
-      vim.g.ale_lint_on_text_changed = 'never'
-      vim.g.ale_disable_lsp = 1
-
-      -- Virtual text settings
-      vim.g.ale_virtualtext_cursor = 1
-      vim.g.ale_virtualtext_prefix = '❯❯ '
-
-      -- Linters configuration
-      vim.g.ale_linters = {
-        python = { 'ruff' },
-        javascript = { 'eslint' },
-        typescript = { 'eslint' },
-        graphql = { 'eslint' },
-      }
-
-      -- Fixers configuration
-      vim.g.ale_fixers = {
-        ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-        python = { 'black', 'isort'},
-        lua = {},
-        -- Enable these if you work with JavaScript/TypeScript
-        -- javascript = { 'prettier', 'eslint' },
-        -- typescript = { 'prettier', 'eslint' },
-      }
-
-      -- Additional settings for better UX
-      vim.g.ale_floating_preview = 1
-      vim.g.ale_hover_to_floating_preview = 1
-      vim.g.ale_detail_to_floating_preview = 1
-    end
-  },
-  {
     'sedm0784/vim-resize-mode',
     config = function()
       -- When vim is resized, equalize the splits
