c409dade0c05dbeda352facc3de74fd56426bc33
jrhoads <jrhoads@datacite.org>
2025-08-20 23:35:45 +0200

    refactor: Remove unused UI plugins modicator, mini.map, vim-maximizer

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -28,35 +28,6 @@ return {
       })
     end,
   },
-  -- modicator (auto color line number based on vim mode)
-  {
-	  "mawkler/modicator.nvim",
-    enabled = false,
-	  -- dependencies = "scottmckendry/cyberdream.nvim",
-	  init = function()
-		  -- These are required for Modicator to work
-		  vim.o.cursorline = true
-		  vim.o.number = true
-		  vim.o.termguicolors = true
-	  end,
-	  opts = {},
-  },
-  {
-	  'echasnovski/mini.map',
-	  version = "*",
-	  config = function()
-		  local map = require('mini.map')
-		  local gen_integr = map.gen_integration
-		  map.setup({
-			  integrations = { gen_integr.builtin_search(), gen_integr.gitsigns() },
-			  symbols = { encode = map.gen_encode_symbols.dot('3x2') },
-			  window = {
-				  show_integration_count = false,
-			  }
-		  })
-		  vim.keymap.set('n', '<leader><right>', ":lua MiniMap.toggle()<CR>", { silent = true })
-	  end
-  },
   { 'folke/twilight.nvim' },
   {
 	  "folke/zen-mode.nvim",
@@ -72,15 +43,6 @@ return {
 		  require("zen-mode").setup {
 			  vim.keymap.set('n', '<leader>m', ":ZenMode<CR>", { silent = true }),
 		  }
-	  end
-  },
-  {
-	  'szw/vim-maximizer',
-	  config = function()
-		  vim.keymap.set('n', '<leader>w', ":MaximizerToggle<CR>", { silent = true })
-		  vim.keymap.set('n', '<C-Space>', ":MaximizerToggle<CR>", { silent = true })
-		  vim.keymap.set('v', '<C-Space>', ":MaximizerToggle<CR>gv", { silent = true })
-		  vim.keymap.set('i', '<C-Space>', "<C-o>:MaximizerToggle<CR>", { silent = true })
 	  end
   },
   { 'Yggdroot/hiPairs' },
